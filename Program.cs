e483e462d8c911d08a08431af2b39ac29b917526
vijay <vijay@VJDELL>
2023-04-01 22:41:37 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter you name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello {0}", name);
+            Console.ReadLine();
+        }
+    }
+}
