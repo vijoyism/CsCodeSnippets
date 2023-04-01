f259c0cfaf5f4e70aeb9d874dbb481388768c42b
Jay balkawade <vijaybalkawade@live.com>
2023-04-01 22:56:54 +0530

    Single half triangle pattern

---
+++ PrintPattern1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintPattern1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 5;
+            for(int j = 0; j < i; j++)
+            {
+                for(int k = 0; k <= j; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
