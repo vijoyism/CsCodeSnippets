847ef11900204cdfa611d61afa1a916997aff9ef
Jay balkawade <vijaybalkawade@live.com>
2023-04-12 09:39:43 +0530

    Pattern 2 and use of partial class

---
+++ UseOfPartialClass/Classes/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UseOfPartialClass.Classes
+{
+    partial class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+    }
+
+    //This can be a part of another assembly with public access specifier
+    partial class Employee
+    {
+        public string LastName { get; set; } = string.Empty;
+
+        public void PrintData(Employee employee)
+        {
+            Console.WriteLine(employee.EmployeeId);
+            Console.WriteLine(employee.FirstName);
+            Console.WriteLine(employee.LastName);
+        }
+
+    }
+}
---
+++ UseOfPartialClass/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UseOfPartialClass.Classes;
+
+namespace UseOfPartialClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.EmployeeId = 1;
+            employee.FirstName = "Vijay";
+            employee.LastName = "Balkawade";
+
+            employee.PrintData(employee);
+            Console.ReadLine();
+        }
+    }
+}
