e34aa112d691c1d1598501474f6c72bf35eae550
Jay balkawade <vijaybalkawade@live.com>
2023-04-27 22:14:17 +0530

    Use of linq on list of integers

---
+++ LinqDemo/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> lstNumber = new List<int>();
+            lstNumber.Add(10);
+            lstNumber.Add(21);
+            lstNumber.Add(30);
+            lstNumber.Add(44);
+            lstNumber.Add(57);
+            lstNumber.Add(60);
+            lstNumber.Add(71);
+            lstNumber.Add(81);
+            lstNumber.Add(90);
+            lstNumber.Add(102);
+
+            var lstEvenNumners = lstNumber.Where(i => i % 2 == 0);
+            Console.WriteLine("The even numbers in the list are:");
+            foreach(int value in lstEvenNumners)
+            {
+                Console.WriteLine(value);
+            }
+            var sumOfEvenNumbers =lstNumber.Where(i => i % 2 == 0).Sum();
+            Console.WriteLine($"Addition of even numbers from the list is {sumOfEvenNumbers}.");
+
+            var lstOddNumbers = lstNumber.Where(i => i % 2 != 0);
+            Console.WriteLine("The odd numbers in the list are:");
+            foreach(int value in lstOddNumbers)
+            {
+                Console.WriteLine($"{value}");
+            }
+            var sumOfOddNumbers = lstNumber.Where(i => i % 2 != 0).Sum();
+            Console.WriteLine($"Addition of odd numbers from the list is {sumOfOddNumbers}.");
+
+            var averageOfOddNumbers = lstOddNumbers.Average();
+            Console.WriteLine($"Average of odd numbers is:{averageOfOddNumbers} ");
+
+            var lstNumbersInDescendingOrder = lstNumber.OrderByDescending( n => n);
+            Console.WriteLine("The list in descending order:");
+            foreach(int value in lstNumbersInDescendingOrder) { Console.WriteLine(value); }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LinqDemo/ReadMe.txt
@@ -0,0 +1,8 @@
+﻿Create a console application with a list of integers
+Perform below operations
+1. Find the even numbers from the list
+2. Find the odd numbers from the list
+3. Find the sum of even numbers from the list
+4. Find the sum of odd numbers from the list
+5. Find the average of odd numbers in the list
+6. Sort the list in descending order and print on the console
