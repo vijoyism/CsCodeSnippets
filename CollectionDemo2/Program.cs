d8608695e5ed9069bf9a913a9f2627f39b3c0d3c
Jay balkawade <vijaybalkawade@live.com>
2023-04-27 22:57:52 +0530

    Get number array and create a dictionary to store occurences

---
+++ CollectionDemo2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionDemo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numberArray = { 1, 2, 3, 2, 2, 52, 6, 7, 5, 3, 5, 7, 8, 9, 9, 7, 6, 5, 52, 1, 7 };
+            Dictionary<int, int> numberOccurences = CountInstances(numberArray);
+            foreach (KeyValuePair<int, int> value in numberOccurences)
+            {
+                Console.WriteLine($"Key:{value.Key} and value:{value.Value}");
+            }
+            Console.ReadLine();
+        }
+
+        public static Dictionary<int, int> CountInstances(int[] inputArray)
+        {
+            Dictionary<int, int> instancesDict = new Dictionary<int, int>();
+            foreach (int item in inputArray)
+            {
+                if (instancesDict.ContainsKey(item))
+                {
+                    instancesDict[item] += 1;
+                }
+                else
+                {
+                    instancesDict[item] = 1;
+                }
+            }
+            return instancesDict;
+        }
+    }
+}
---
+++ CollectionDemo2/ReadMe.txt
@@ -0,0 +1,2 @@
+﻿Write a c# method that takes an array of integers and returns a dictionary<int,int> where the keys are the unique 
+integers in the array and the values are the number of instances of that integer.
